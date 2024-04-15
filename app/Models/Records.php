2e709f04ac354aaba0e0aebf597e60bdf0d02579
Maarma <Maarma25@gmail.com>
2024-04-15 22:17:57 +0300

    records mcr

---
+++ app/Models/Records.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Records extends Model
+{
+    use HasFactory;
+    protected $table = 'records';
+    protected $fillable = ['name', 'author', 'price', 'image', 'tracks'];
+}
